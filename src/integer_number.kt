e7f1d2bccc88ed9ce3cc8d239496b776711daa53
efendykn27 <efendikn27@gmail.com>
2020-09-21 20:52:24 +0700

    Var Val

---
+++ src/integer_number.kt
@@ -0,0 +1,26 @@
+fun main(){
+
+    var age:Byte = 21
+    var height:Int=166
+    var distance:Short = 2010
+    var balance:Long = 1000000000000000L
+
+    println(age)
+    println(height)
+    println(distance)
+    println(balance)
+
+    //convert tipe data
+    val bebas:Int = 500000000
+    var umur:Short = bebas.toShort()
+    var jarak:Int = bebas.toInt()
+    var tinggi:Byte = bebas.toByte()
+    var float1:Float=bebas.toFloat()
+
+    println(tinggi)
+    println(jarak)
+    println(umur)
+
+
+
+}
---
+++ src/tidat_char.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    var char1: Char='u'
+    var varchar1:String="ular"
+    var hewan:String="""
+        #Sapi
+        #Kodok
+        #Cacing
+    """.trimMargin(marginPrefix ="#" )
+    println(hewan)
+    println(hewan+" "+varchar1)
+
+    var depan:String="Efendi"
+    var blkg:String="mbuh"
+    println("$depan $blkg")
+
+    var benar:Boolean=true
+    var salah:Boolean=false
+}
